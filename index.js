cfc1b05446f85dbaa9ec9dc3564eded424320751
Rolex Omondi <rolexomondi46@gmail.com>
2023-03-13 14:27:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+let currentUser = "Rolex";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+// similar to : let shortGreeting = `Welcome, ${currentUser.charAt(0))}!`
+console.log(shortGreeting);
